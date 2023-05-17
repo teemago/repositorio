e6fa77a79615ddc4dd87c40aa9470bcf57933991
unknown <zenaboa7@gmail.com>
2023-05-17 11:46:32 -0300

    A

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 const buttonList = document.querySelector("#listar-aulas");
+const buttonHash = document.querySelector("#listar-hash");
+const input = document.querySelector("#input");
 const divResult = document.querySelector("#resultado");
 
 buttonList.addEventListener("click", async function () {
-    const result = await fetch("http://localhost/ams/")
+    const result = await fetch("http://localhost/ams/dados-aulas")
         .then((response) => {
             return response.json();
         });
-    montarHTMLaulas(result);
+    divResult.innerHTML = "";
+    const show = result.map(x => {
+        const div = document.createElement("div");
+        div.innerHTML = x.nome;
+        divResult.appendChild(div);
+    })
+})
+
+buttonHash.addEventListener("click", async function (event) {
+    const result = await fetch("http://localhost/ams/hash/" + input.value)
+    .then((response) => {
+        return response.json();
+    });
+    console.log(result);
+    const div = document.createElement("div");
+    divResult.innerHTML = "";
+    div.innerHTML = result;
+    divResult.appendChild(div);
+    // const show = result.map(x => {
+    //     div.innerHTML = x;
+    //     divResult.appendChild(div);
+    // })
+
 
 })
 
 montarHTMLaulas = (aulas) => {
-    const divResult2 = document.createElement("div"); 
+    const divResult2 = document.createElement("div");
     aulas.forEach((aula) => {
         const button = document.createElement("button");
         button.innerHTML = aula;
